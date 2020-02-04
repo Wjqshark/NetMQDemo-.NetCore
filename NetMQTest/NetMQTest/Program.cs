bc67f29178e19b238ff4b5eeadff00e56db4ecef
王佳祺 <719539302@qq.com>
2020-02-04 19:19:03 +0800

    NetMQ .NetCoreDemo

---
+++ NetMQTest/NetMQTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace NetMQTest
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "NetMQ Weather Update Server";
+
+            bool stopRequested = false;
+
+            // Wire up the CTRL+C handler
+            Console.CancelKeyPress += (sender, e) => stopRequested = true;
+
+            Console.WriteLine("Publishing weather updates...");
+
+            using (var publisher = new PublisherSocket())
+            {
+                publisher.Bind("tcp://127.0.0.1:5556");
+
+                var rng = new Random();
+
+                while (!stopRequested)
+                {
+                    int zipcode = rng.Next(0, 99999);
+                    int temperature = rng.Next(-80, 135);
+                    int relhumidity = rng.Next(0, 90);
+
+                    publisher.SendFrame($"{zipcode} {temperature} {relhumidity}");
+                }
+            }
+        }
+    }
+}
